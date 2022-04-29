f023eb4d53a155a58a6a98d2e812fa01950ee50a
Maurycy Z <10maurycy10@gmail.com>
2022-04-29 16:25:00 -0700

    init

---
+++ updateall.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+PKG=$(find update.d -type f | grep -v detect)
+while IFS= read -r line; do
+	echo -e "\e[1mUpdating $line...\e[0m"
+	skip="0"
+	if [[ -f "$line.detect" ]]; then
+		sh $line.detect 2> /dev/null
+		skip="$?"
+	fi
+	if [[ $skip == 0 ]]; then
+		if sh -- "$line"; then
+			echo -e "\e[1;32m$line updated\e[0m"
+		else
+			echo -e "\e[1;31mError updating $line\e[0m"
+			exit 1
+		fi < /dev/tty
+	else
+		echo -e "\e[1mUpdating $line skiped\e[0m"
+	fi
+done <<< $PKG
+
