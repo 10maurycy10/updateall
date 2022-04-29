414343c35425e1bb964ba6c723ceacc940970e14
Maurycy Z <10maurycy10@gmail.com>
2022-04-29 16:44:31 -0700

    make updateall.sh slightly nicer

--- updateall.sh
+++ updateall.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 PKG=$(find update.d -type f | grep -v detect)
 while IFS= read -r line; do
-	echo -e "\e[1mUpdating $line...\e[0m"
+	echo -en "\e[1mUpdating $line... \e[0m"
 	skip="0"
 	if [[ -f "$line.detect" ]]; then
 		sh $line.detect 2> /dev/null
 		skip="$?"
 	fi
 	if [[ $skip == 0 ]]; then
+		echo ""
 		if sh -- "$line"; then
 			echo -e "\e[1;32m$line updated\e[0m"
 		else
@@ -15,7 +16,7 @@ while IFS= read -r line; do
 			exit 1
 		fi < /dev/tty
 	else
-		echo -e "\e[1mUpdating $line skiped\e[0m"
+		echo -e "\e[1;33mskipped\e[0m"
 	fi
 done <<< $PKG
 
